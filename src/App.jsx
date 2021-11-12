c9cc51b09de96a15698769f843be55e9de40e41b
Harshal Lele <harshallele97@gmail.com>
2021-11-12 23:34:23 +0530

    added auth method

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-
+  const baseUrl = `http://localhost:4000`;
   const [user,setUser] = useState('');
   const [pass,setPass] = useState('');
   
-  const login = async () => {
-    let body = {user,pass};
+  const login = async (scope) => {
+    if(user == '' || pass == '') {
+      console.log('user/pass empty');
+      return;
+    }
 
-    fetch('/login',{
+    let reqBody = {user,pass,scope};
+
+    fetch(`${baseUrl}/auth`,{
       method: 'POST',
-      headers: {"Content-type": "application/json;charset=UTF-8"},
-      body: JSON.stringify(body)
+      headers: {'Content-type': 'application/json;charset=UTF-8'},
+      body: JSON.stringify(reqBody)
     })
     .then(r => r.json())
     .then(res => {
@@ -30,7 +35,8 @@ function App() {
         <div className='text-2xl md:text-5xl mb-8'>Login</div>
         <input type='email' className='w-11/12 text-lg md:text-xl px-2 py-2 border-2 active:border-green-300 my-2' placeholder='Username...' id='user' value={user} onChange={(e) => {setUser(e.target.value)}}/>
         <input type='password' className='w-11/12 text-lg md:text-xl px-2 py-2 border-2 active:border-green-300' placeholder='Password...' id='passwd' value={pass} onChange={(e) => {setPass(e.target.value)}}/>
-        <button className='my-2 border-2 border-green-800 rounded-md px-5 py-2 active:text-white active:bg-green-700' onClick={login}>Login</button>
+        <button className='my-2 border-2 border-green-800 rounded-md px-5 py-2 mx-2 active:text-white active:bg-green-700' onClick={() => login('login')}>Login</button>
+        <button className='my-2 border-2 border-green-800 rounded-md px-5 py-2 mx-2 active:text-white active:bg-green-700' onClick={() => login('signup')}>Sign Up</button>
       </div>
     </div>
   );
